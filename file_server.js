8a9edbc1d9b103b56ca1a87c1df85587376034c1
terrywh <terry.wuhao@gmail.com>
2017-01-10 13:22:15 +0800

    fix fs ref

--- file_server.js
+++ file_server.js
@@ -5,31 +5,29 @@ const path = require("path"),
 	mtypes = require("mime-types");
 
 module.exports = function(www) {
-	www = path.normalize(www)
+	www = path.normalize(www);
 	return function*(next) {
-		let status = 302,
-			path   = this.path,
-			fsys   = wpPattern.test(www + "/" + path) ? wpFileSys: fs
+		let status = 302, path = this.path;
 		while(status === 302) {
 			status = yield function(done) {
-				fsys.stat(www + path, function(error, stats) {
+				fs.stat(www + path, function(error, stats) {
 					done(null, error ? (error.code === "ENOENT" ? 404 : 403) : (stats.isDirectory()? 302 : 200) )
 				});
 			};
 			if(status === 302) {
 				if(path.substr(-1) === '/') {
-					path += "index.html"
+					path += "index.html";
 				}else{
-					path += "/index.html"
+					path += "/index.html";
 				}
 			}else if(status === 200) {
-				this.type = mtypes.lookup(path) || "application/octet-stream"
-				this.body = fsys.createReadStream(www + path)
+				this.type = mtypes.lookup(path) || "application/octet-stream";
+				this.body = fsys.createReadStream(www + path);
 			}else if(status === 403){
-				this.status = status
+				this.status = status;
 			}else{
-				yield next
+				yield next;
 			}
-		}
+		};
 	};
 };
