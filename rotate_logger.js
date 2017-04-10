94021a4a951848d7b731881ae9840c7233a2b0dc
TerryWu <terrywh@users.noreply.github.com>
2017-04-10 18:48:02 +0800

    Update rotate_logger.js

--- rotate_logger.js
+++ rotate_logger.js
@@ -1,20 +1,70 @@
 "use strict";
 const fs = require("fs"),
-	util = require("util");
+	path = require("path"),
+	levels = ["trace", "debug", "information", "notice", "warning", "error", "critical", "fatal"]; // 与 c++ 对应
 
-module.exports = function(file) {
-	let w = fs.createWriteStream(file, {flags: "a+"}),
-		l = {path: file};
-	l.rotate = function() {
-		w.end();
-		w = fs.createWriteStream(file, {flags: "a+"});
-	};
-	l.print = function() {
-		w.write(util.format.apply(util, arguments) + "\n");
-	};
-	l.fatal = function() {
-		w.write(util.format.apply(util, arguments) + "\n");
-		process.exit();
-	};
-	return l;
+let logger = {};
+
+logger.rotate = function() {
+	if(logger._fs === process.stdout) return;
+
+	if(logger._fs) logger._fs.end();
+	logger._fs = fs.createWriteStream(
+		logger._file,
+		{flags: "a+"});
+};
+
+logger._cache = [];
+logger._write = function() {
+	let data = logger._cache.shift();
+	if(!data) return;
+	let date = new Date();
+	date = "["
+			+ date.getFullYear() + "-"
+			+ ("0" + (date.getMonth()+1)).substr(-2) + "-"
+			+ ("0" + date.getDate()).substr(-2) + " "
+			+ ("0" + date.getHours()).substr(-2) + ":"
+			+ ("0" + date.getMinutes()).substr(-2) + ":"
+			+ ("0" + date.getSeconds()).substr(-2) + "] ";
+	logger._fs.cork();
+	logger._fs.write(date);
+	logger._fs.write("(" + data.shift() + ") ");
+
+	logger._fs.write(data.shift());
+	logger._fs.write("\n", function() {
+		if(logger._cache.length) {
+			setImmediate(logger._write);
+		}
+	});
+	logger._fs.uncork();
+};
+
+module.exports = function(file, l) {
+	if(logger._fs) return logger;
+	if(typeof l === "string") {
+		l = levels.indexOf(l);
+	}else if(typeof l !== "number") {
+		l = 0;
+	}
+	if(file) {
+		logger._file = file.startsWith("/") ? file : path.join(__dirname, "/../log/", file);
+		logger.rotate();
+	}else{
+		logger._fs = process.stdout;
+	}
+
+	levels.forEach(function(level, index) {
+		if(index >= l) {
+			logger[level] = function() {
+				logger._cache.push([level, Array.from(arguments).join(" ")]);
+				logger._write();
+			};
+		}else{
+			logger[level] = function() {};
+		}
+	});
+	logger["info"] = logger["information"];
+	logger["warn"] = logger["warning"];
+	logger["fail"] = logger["error"];
+	return logger;
 };
