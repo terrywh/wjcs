4e03b1587e3fa5f902aae2b17e735cfc973cc3c9
TerryWu <terrywh@users.noreply.github.com>
2019-06-06 17:58:35 +0800

    Create console_color.js

---
+++ console_color.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const colorlog = function (app, module, action) {
+    let argv = Array.from(arguments);
+    
+    argv.splice(0, 2, "%c[%c" + app + "%c] [%c" + module + "%c/%c" + action +"%c]",
+        "color: #999", "color: #CC6666", "color: #999",
+        "color: #6666CC", "color: #999", "color: #6666CC", "color: #999");
+    console.debug.apply(console, argv);
+};
